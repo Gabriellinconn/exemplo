a8ec173c170ee12679b93dad2f5ba0c3e54bffe5
Auto Logon <autologon@CSED.LAB>
2024-03-05 21:12:27 -0300

    Primeira alteração

---
+++ exercicios/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+
+//Console.WriteLine("Olá Mundo C#!");
+
+//Console.WriteLine("Digite uma mensagem");
+//string mensagem = Console.ReadLine();
+//Console.WriteLine (mensagem);
+
+// Console.WriteLine("Digite uma numero a");
+// string varA = Console.ReadLine();
+
+// Console.WriteLine("Digite uma numero b");
+// string varB = Console.ReadLine();
+
+// int a = Convert.ToInt32(varA);
+// int b = Convert.ToInt32(varB);
+
+// int resultado = a + b;
+
+// Console.WriteLine ("O resultado é: " + resultado);
+
+// Console.WriteLine("Digite uma numero a");
+// int a = Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Digite uma numero b");
+// string varB = Console.ReadLine();
+
+
+// int b = Convert.ToInt32(varB);
+
+
+// if((a+b)>=6){
+//     Console.WriteLine("Aluno Aprovado");
+// }
+// else {
+//      Console.WriteLine("Aluno Reprovado");
+// }
+
+// Console.WriteLine("Digite uma número");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine ("O antecessor é "+ (a-1) + " E o sucessor é "+ (a+1));
---
+++ exercicios/atv1.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Digite a altura de seu retângulo: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a largura do retângulo: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A Área do seu retângulo é: {l*a}");
